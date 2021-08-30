3dae14a7e36373d4f79431ee218ec5708c889d44
stwertnikcoding <chransondub@gmail.com>
2021-08-30 12:04:21 -0600

    modified SearchBar Component to take onSubmit function via props

--- src/components/results.js
+++ src/components/results.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-
 import Logo from './logo';
-import SearchBar from './searchBar'
+import SearchBar from './searchBar';
 
- class Results extends Component {
-  render() {
-    return (
-      <div>
-        <Logo size={55} />
-        <SearchBar />
-      </div>
-    );
-  }
+class Results extends Component {
+
+    handleSearchBarSubmit(query) {
+        console.log(query);
+    }
+
+    render() {
+        return (
+            <div>
+                <Logo size={55}/>
+                <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+            </div>
+        )
+    }
 }
 export default Results;
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-
+import { Field, reduxForm} from 'redux-form';
 import { withRouter } from 'react-router-dom';
-
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
-        console.log('trying to handle submit for query', query);
-
-        this.props.history.push('/results');
-        
+        this.props.onSubmit(query);
     }
 
     renderInput(field) {
         return <input type="text" placeholder="Search DailySmarty" {...field.input} />
     }
-
     render() {
-
         const { handleSubmit } = this.props;
-
         return (
             <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <Field name="query" component={this.renderInput}/>
@@ -27,11 +19,9 @@ class SearchBar extends Component {
         )
     }
 }
-
 SearchBar = reduxForm({
     form: 'searchBar'
 })(SearchBar);
-
 SearchBar = withRouter(SearchBar);
 
 export default SearchBar;
